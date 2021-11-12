0b6ba32253566d6fba8dc4987b132b4867fd14cf
aliyuarba <arba313@gmail.com>
2021-11-12 15:22:01 +0700

    remove auto hide navbar, add smooth scrolling

--- app.js
+++ app.js
@@ -30,17 +30,13 @@ $("#btnmenu").click(function(){
 $("main").click(function(){
   if($("#menu").is(":visible")){
     $("#menu").removeClass("show");
+    $("#line1").css({
+      "transform": "translate(-50%, -50%)", 
+      "top": "30%"});
+    $("#line2").css({
+        "left": "50%"});
+    $("#line3").css({
+      "transform": "translate(-50%, -50%)", 
+      "top": "70%"});
   }
 })
-
-var prev = $(window).scrollTop();
-window.onscroll = function(){
-  var current = $(window).scrollTop();
-    if (prev < current && current > 379){
-      $("nav").css("top", "-45px");
-    }
-    else {
-      $("nav").css("top", "0")
-    }
-  prev = current;
-}
