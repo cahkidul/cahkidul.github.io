3c9f0cd383faeceb6335822df53fe2466d1a8345
aliyuarba <arba313@gmail.com>
2021-11-12 15:15:46 +0700

    make navbar responsive

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ $("#btnmenu").click(function(){
   if($("#line2").css("left") === "135px"){
     $("#line1").css({
       "transform": "translate(-50%, -50%)", 
-      "top": "25%"});
+      "top": "30%"});
     $("#line2").css({
         "left": "50%"});
     $("#line3").css({
       "transform": "translate(-50%, -50%)", 
-      "top": "75%"});
+      "top": "70%"});
   }
   else {
     $("#line1").css({
@@ -27,6 +27,20 @@ $("#btnmenu").click(function(){
   }
 })
 
-// $("#logo").click(function(){
-//   alert($("#menu").height());
-// })
+$("main").click(function(){
+  if($("#menu").is(":visible")){
+    $("#menu").removeClass("show");
+  }
+})
+
+var prev = $(window).scrollTop();
+window.onscroll = function(){
+  var current = $(window).scrollTop();
+    if (prev < current && current > 379){
+      $("nav").css("top", "-45px");
+    }
+    else {
+      $("nav").css("top", "0")
+    }
+  prev = current;
+}
